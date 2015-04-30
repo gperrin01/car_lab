275855837298d329fadd4b61faf752114d6c2c38
Guillaume Perrin <gperrin01@gmail.com>
2015-04-30 17:18:10 +0100

    passed test 4

--- src/car.js
+++ src/car.js
@@ -5,6 +5,7 @@ function Car(make, model, year, color){
   // this.color = ;
   this.state = 'off';
   this.previousOwners = [];
+  this.currentOwner = 'Manufacturer';
 }
 
 Car.prototype.sale = function(newOwner){
